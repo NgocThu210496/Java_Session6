af167b197a4a4744ac3721b1492c8b18117f324d
Thu Nguyen <ngocthu6778@gmail.com>
2023-09-24 14:01:48 +0900

    edit exercise_5

--- src/exercise_5/StudentManagenment_Main.java
+++ src/exercise_5/StudentManagenment_Main.java
@@ -6,6 +6,7 @@ public class StudentManagenment_Main {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
         StudentManagenment[] studentManagenments= new StudentManagenment[100];
+        int currentIndex=0;
         StudentManagenment student1 = new StudentManagenment("SV1", "Nguyen Thu", 18, true, "Da Nang" , 754375567);
         StudentManagenment student2 = new StudentManagenment("SV2", "Nguyen A", 20, false, "Ha Noi", 908765212);
         StudentManagenment student3 = new StudentManagenment("SV3", "Nguyen B", 20, false, "HCM", 908765212);
@@ -16,7 +17,6 @@ public class StudentManagenment_Main {
         studentManagenments[3]=student4;
 
         int choice;
-        int count=0;
         do {
             System.out.println("*********MENU********");
             System.out.println("1. Hiển thị danh sách tất cả học sinh");
@@ -30,53 +30,52 @@ public class StudentManagenment_Main {
             switch (choice){
                 case 1:
                     System.out.println("1. Hiển thị danh sách tất cả học sinh");
-                    for (StudentManagenment student : studentManagenments) {//duyệt qua mảng studentManagenments
-                        if (student != null) {//kiểm tra xem student có khác null không (để tránh hiển thị thông tin của các vị trí trong mảng chưa được khởi tạo)
-                            student.displayData(); // Hiển thị thông tin sinh viên
-                        }
+                    for(int i=0;i<currentIndex; i++){
+                        studentManagenments[i].displayData();
                     }
                     break;
                 case 2:
                     System.out.println("2. Thêm mới học sinh");
-                    StudentManagenment newStudent= new StudentManagenment();
-                    newStudent.inputData(scanner);
-                    for (int i = 0; i < 5; i++) {
-                        if (studentManagenments[i] == null) {
-                            studentManagenments[i] = newStudent;
-                            break;
-                        }
-                    }
+                    studentManagenments[currentIndex]=new StudentManagenment();
+                    studentManagenments[currentIndex].inputData(scanner);
+                    currentIndex++;
                     break;
                 case 3:
                     System.out.println("3.Nhập mã sinh viên cần sửa: ");
-                    StudentManagenment editStudent= new StudentManagenment();
                     String id= scanner.nextLine();
-                    editStudent.setStudentId(id);
-                    System.out.println("Tên SV: ");
-                    editStudent.setStudentName(scanner.nextLine());
-                    System.out.println("Tuổi SV: ");
-                    editStudent.setAge(Integer.parseInt(scanner.nextLine()));
-                    System.out.println("Giới tính: ");
-                    editStudent.setSex(Boolean.parseBoolean(scanner.nextLine()));
-                    System.out.println("Địa chỉ: ");
-                    editStudent.setAddress(scanner.nextLine());
-                    System.out.println("Số điện thoại: ");
-                    editStudent.setPhone(Integer.parseInt(scanner.nextLine()));
-                    for (int i = 0; i < 100; i++) { // duyệt mảng
-                        if (studentManagenments[i] !=null && studentManagenments[i].getStudentId()==id){ // nếu mã sinh viên nhập vào bằng mã sinh viên trong mảng
-                            studentManagenments[i]= editStudent;
+                   //tim chi so phan tu can sua trong mang
+                    int updateIndex=-1; //neu k tim thay phan tu
+                    for(int i=0;i<currentIndex;i++ ){
+                        if(studentManagenments[i].getStudentId().equals(id)){
+                            updateIndex=i;
+                            break;
                         }
                     }
+                    if(updateIndex>=0){ //chi so phan tu can cap nhat
+                        System.out.println("Ten SV");
+                        studentManagenments[updateIndex].setStudentName(scanner.nextLine());
+                        System.out.println("Tuoi");
+                        studentManagenments[updateIndex].setAge(Integer.parseInt(scanner.nextLine()));
+                        System.out.println("Gioi tinh");
+                        studentManagenments[updateIndex].setSex(Boolean.parseBoolean(scanner.nextLine()));
+                        System.out.println("Dia chi");
+                        studentManagenments[updateIndex].setAddress(scanner.nextLine());
+                        System.out.println("So dien thoai");
+                        studentManagenments[updateIndex].setPhone(Integer.parseInt(scanner.nextLine()));
+                    }else {
+                        System.out.println("Ma SV Khong ton tai");
+                    }
+
                     break;
                 case 4:
                     System.out.println("Nhập mã sinh viên cần xoá: "); // nhập mã sinh viên cần xoá
                     String id1= scanner.nextLine();
-                    for (int i = 0; i < count; i++) {
+                    for (int i = 0; i < currentIndex; i++) {
                         if (studentManagenments[i].getStudentId().equals(id1)){ // nếu mã sinh viên nhập vào bằng mã sinh viên trong mảng
-                            for (int j = i; j < count-1; j++) { // duyệt mảng từ vị trí mã sinh viên nhập vào đến vị trí cuối cùng
+                            for (int j = i; j < currentIndex-1; j++) { // duyệt mảng từ vị trí mã sinh viên nhập vào đến vị trí cuối cùng
                                 studentManagenments[j]= studentManagenments[j+1]; // gán phần tử tiếp theo cho phần tử hiện tại
                             }
-                            count--;
+                            currentIndex--;
                         }
                     }
                     break;
